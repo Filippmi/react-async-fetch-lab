14d3708487430bb0355d3ecc3c776af642a3eb81
Filipp Minayev <Filippmi@gmail.com>
2021-03-23 22:44:08 -0600

    commit tree bf270a4721db9848bd01d6dde63b1f9de6a1590c

--- src/App.js
+++ src/App.js
@@ -1,1 +1,26 @@
-// create your App component here
+import React from 'react';
+
+class App extends React.Component {
+
+  state = {
+    peopleInState: []
+  }
+
+  render() {
+    return(
+      <div>
+        
+      </div>
+    )
+  }
+  componentDidMount() {
+    fetch('http://api.open-notify.org/astros.json')
+    .then(response => response.json())
+    .then(data => {
+      this.setState({peopleInState: data.people})
+    })
+      
+  }
+}
+
+export default App;
